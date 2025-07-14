e6851cfb00439710e4fb0c1f3e5e25fdb0671c50
Fernando Nina <Fernandonina611@gmail.com>
2025-07-14 10:47:00 -0400

    chore: ajustes form

--- src/components/Patient/PatientDetails.tsx
+++ src/components/Patient/PatientDetails.tsx
@@ -15,6 +15,17 @@ export const PatientDetails = ({patient}: PatientDetailsProps) => {
             <PatientDetailItem label="Email" value={patient.email} />
             <PatientDetailItem label="Fecha Alta" value={date} />
             <PatientDetailItem label="Síntomas" value={patient.symptoms} />
+
+            <div className="flex justify-between gap-3 mt-10">
+                <button className="bg-indigo-600 py-2 px-10 hover:bg-indigo-700 transition-colors delay-100 cursor-pointer
+                text-white font-bold uppercase rounded-lg">
+                    Editar
+                </button>
+                <button className="bg-red-600 py-2 px-10 hover:bg-red-700 transition-colors delay-100 cursor-pointer
+                text-white font-bold uppercase rounded-lg">
+                    Eliminar
+                </button>
+            </div>
         </div>
     )
 }
